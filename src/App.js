e746b008b0fc76cfa38a5ae7b0fb7f9377a30345
Drake Bott <bott.da@gmail.com>
2018-10-01 19:08:20 -0600

    Import data and Card class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import DistrictRepository from './helper';
+import kinderData from './data/kindergartners_in_full_day_program.js';
+import Card from './Card';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      stats: new DistrictRepository(kinderData).stats
+    }
+  }
+
   render() {
+    const locations = Object.keys(this.state.stats);
+    const cards = locations.map( (place, index) => {
+      return (<Card
+        stats={this.state.stats[place]}
+        location={place}
+        key={index}/>);
+    });
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div className='Cards'>
+      { cards }
+      </div>
     );
   }
 }
