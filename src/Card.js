ef42a19115dfc1b588f669b43da12895b41c443b
Drake Bott <bott.da@gmail.com>
2018-10-01 19:09:51 -0600

    Remove console log

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 class Card extends Component {
   render() {
-    console.log(this.props.stats);
     return (
       <div>{this.props.location}</div>
     );
