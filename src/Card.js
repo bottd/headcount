30224a24f8aa7f9963582caabcee609f13274fa8
Drake Bott <bott.da@gmail.com>
2018-10-01 19:26:12 -0600

    Map over data to create card components to add to App page

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import './Card.css';
 
 class Card extends Component {
   render() {
+    const yearData = Object.keys(this.props.stats).map((year, index) => {
+      const percentage = this.props.stats[year];
+      let status = 'greater';
+      if (percentage < 0.5) status = 'lessThan';
+      return(<li key={index} className={status}>
+        <span className='year'>{year}: </span>
+        <span className='yearValue'>{percentage}</span>
+      </li>);
+    });
     return (
-      <div>{this.props.location}</div>
+      <div className='DistrictData'>
+        <h3>{this.props.location}</h3>
+        <ul className='yearData'>
+          {yearData}
+        </ul>
+      </div>
     );
   }
 }
